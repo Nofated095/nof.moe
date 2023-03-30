3cb6e0cda52695919559190afc3d8fb9a1329c9c
谷風天音 | 是一个浓度纯度都比较高的柚子厨，因为柚子社天下第一！时不时发柚子的癫。有个能干的妹妹真好，たにかぜ あまね天下第一喵，关注谷風天音谢谢喵，关注谷風天音谢谢喵，谢谢在这里看我发癫，天音妹妹真的好好看喵，单推天音一辈子！谁不推天音就打爆他的宁宁的起爆器！\柚子社/\柚子社/\柚子社/\天音/\天音/\天音/天音，我真的好喜欢你啊，为了你，我要 0721！\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/\たにかぜ あまね/ <49985975+Nofated095@users.noreply.github.com>
2023-03-31 00:17:24 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,14 @@ import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import App from './App';
 import StickyFooter from './StickyFooter';
-import Tabs from './Tabs';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
-      <Tabs />
+
       <StickyFooter />
     </ThemeProvider>
   </React.StrictMode>,
