f58baba8c0be3507fd7760ddd816ff1cdbf5bdf1
Nofated095 <nofated095@outlook.com>
2023-04-03 17:21:30 +0800

    up

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import Grid from '@mui/material/Unstable_Grid2';
+import Avatar from '@mui/material/Avatar';
 import Tabs from './Tabs';
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary">
+    <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://github.com/Nofated095">
+      <Link color="inherit" href="https://nof.moe">
         Nofated
       </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      {new Date().getFullYear()}.
     </Typography>
   );
 }
@@ -23,12 +23,23 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Nofated
-        </Typography>
-        <Typography variant="body1">Forever you will be in my heart.</Typography>
-      </Container>
+        <Grid container spacing={2}>
+          <Grid xs={4}>
+            {/* <img
+              src={`https://cdn.nofated.win/avatarsoss`}
+              srcSet={`https://cdn.nofated.win/avatarsoss`}
+              alt={'Avatar'}
+              loading="lazy"
+              width={128}
+            /> */}
+            <Avatar alt="Nofated" src="https://cdn.nofated.win/avatarsoss" sx={{ width: 128, height: 128 }} />
+          </Grid>
+          <Grid xs={8}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Nofated
+            </Typography>
+          </Grid>
+        </Grid>
         <Tabs />
         <Copyright />
       </Box>
--- src/theme.tsx
+++ src/theme.tsx
@@ -14,6 +14,21 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  typography: {
+    fontFamily: [
+      'Overpass Mono',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
 
 export default theme;
